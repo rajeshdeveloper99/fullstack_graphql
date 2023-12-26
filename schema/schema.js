c43826e613a5f91b3f12187c9fa409d986506666
expertdeveloper5821 <expert.developer.5821@gmail.com>
2023-12-26 17:45:00 +0530

    add mutation for create new users

--- schema/schema.js
+++ schema/schema.js
@@ -1,27 +1,34 @@
-
-import { gql } from "apollo-server";
-
+import {gql} from "apollo-server"
 const typeDefs = gql`
-  type Query {
-    users: [User]
-    user(id: ID!): User
-    quotes: [Quote]
-    iquote(by: ID!): [Quote]
-  }
+ type Query{
+    users:[User]
+    user(id:ID!):User
+    quotes:[Quote]
+    iquote(by:ID!):[Quote]
+ }
+ type User{
+     id:ID!
+     firstName:String
+     lastName:String
+     email:String
+     password:String
+     quotes:[Quote]
+ }
+ type Quote{
+     name:String
+     by:ID
+ }
 
-  type User {
-    id: ID!
-    firstName: String
-    lastName: String
-    email: String
-    password: String
-    quotes: [Quote]
-  }
+ type Mutation{
+     signupUserDummy(userNew:UserInput!):User
+ }
 
-  type Quote {
-    name: String
-    by: ID
-  }
-`;
+ input UserInput{
+    firstName:String!
+    lastName:String!
+    email:String!
+    password:String!
+ }
 
+`
 export default typeDefs
